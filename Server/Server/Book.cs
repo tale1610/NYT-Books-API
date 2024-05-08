03c7a409de821bb94f2f45da9c3b2c5156f709dd
taskov_ <taskovvukan@gmail.com>
2024-05-08 18:52:14 +0200

    added client and server

---
+++ Server/Server/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Server
+{
+    public class Book
+    {
+        public string Url { get; set; }
+        public string PublicationDate { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Summary { get; set; }
+    }
+}
